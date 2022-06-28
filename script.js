eac1e5aa5034372085ef2c9779706491efea3e42
Raaid-Latief <raaidlatiefgbr@gmail.com>
2022-06-28 16:20:23 +0200

    4th commit

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 let properties = [{
-    image: "",
-    title: "",
-    location: "",
-    price: "",
-    sqFeet: "",
+    image: "https://i.postimg.cc/W1grGtXk/house-1836070-1920.jpg",
+    title: "Modern House",
+    location: "Crawford",
+    price: "R13 000 pm",
+    sqFeet: "4000",
 
 },
 {
-    image: "",
-    title: "",
-    location: "",
-    price: "",
-    sqFeet: "",
+    image: "https://i.postimg.cc/NGnrZRnX/house-6967908-1920.jpg",
+    title: "Forest Hideaway",
+    location: "Tokai",
+    price: "R12 00 pm",
+    sqFeet: "3200",
 
 },
 {
-    image: "",
-    title: "",
-    location: "",
-    price: "",
-    sqFeet: "",
+    image: "https://i.postimg.cc/26z48mdB/large-home-389271-1280.jpg",
+    title: "Beach House",
+    location: "Strand",
+    price: "R17 000 pm",
+    sqFeet: "4280",
 
 },
 {
-    image: "",
-    title: "",
-    location: "",
-    price: "",
-    sqFeet: "",
+    image: "https://i.postimg.cc/3wHGnpRT/interior-2685517-1920.jpg",
+    title: "Studio Apartment",
+    location: "CBD",
+    price: "R11 000 pm",
+    sqFeet: "2670",
 
 },
 {
-    image: "",
-    title: "",
-    location: "",
-    price: "",
-    sqFeet: "",
+    image: "https://i.postimg.cc/TPjV6hb6/house-1477041-1280.jpg",
+    title: "Influencer Mansion",
+    location: "Claremont",
+    price: "R21 000 pm",
+    sqFeet: "4890",
 
 },
 {
-    image: "",
-    title: "Double Story Beach House",
+    image: "https://i.postimg.cc/PxrwnyhW/house-1867187-1920.jpg",
+    title: "Double Story Beach",
     location: "Strand",
     price: "18 000 pm",
     sqFeet: "4000 sqFt",
@@ -78,7 +78,7 @@ function showProperties(properties) {
     document.querySelector("#propertyList").innerHTML = "";
     pokemonArray.forEach((properties) => {
        document.querySelector("#propertyList").innerHTML += `
-      <img src=${} alt="Front">
+      <img src=${image} alt="Front">
        `;
     })
 }
